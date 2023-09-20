e30e0a392f700bb7ebd100c8fbcc460b2573010d
likith19981211 <sailikithgowd1998@gmail.com>
2023-09-20 18:47:26 +0530

    logs in inventoryservice

--- src/main/java/com/ecommerce/inventoryservice/service/InventoryService.java
+++ src/main/java/com/ecommerce/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.ecommerce.inventoryservice.service;
 import com.ecommerce.inventoryservice.dto.InventoryResponse;
 import com.ecommerce.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +14,16 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode) {
+    public List<InventoryResponse> isInStock(List<String> skuCode) throws InterruptedException {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
