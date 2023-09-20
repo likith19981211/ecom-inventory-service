b769af05725bb351f0acaa1229f34b67e30cb143
likith19981211 <sailikithgowd1998@gmail.com>
2023-09-20 17:31:14 +0530

    api-gateway and keycloak security config

--- src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,13 @@ import com.ecommerce.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
